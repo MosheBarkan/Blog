47566b183e073b4db3e59a5c22843e3c90709360
Moshe Barkan <moshiko226@hotmail.com>
2016-08-14 19:55:27 +0300

    Added some stuff to Martef.js

--- js/martef.js
+++ js/martef.js
@@ -8,18 +8,54 @@ var newPerson = [];
 $("#martefButton").click(function(){
     var innerText = $("#textBox").val();
     if (innerText.length > 0) {
-    divAlive();
-    newPerson[forIndex] = new Person(innerText,18);
-    addElement("Added a new person named : " + newPerson[forIndex].name);
-    forIndex++;
-    $("#textBox").val('');
+       if(!AlreadyIn(newPerson,innerText)) {
+       dieAlive();
+       newPerson[forIndex] = new Person(innerText,18);
+       addElement("Added a new person named : " + newPerson[forIndex].name);
+       forIndex++;
+       $("#textBox").val('');
+      }
+      else {
+        console.log(("This value is already inside the database"));
+        dieAlive();
+        addElement("Taken Already");
+        $(".popup").css("background-color" , "red");
+
+      }
   }
 });
 
 function printAll() {
-  for (var variable in newPerson) {
-    console.log("name: " + newPerson[variable].name);
-    console.log("age: " + newPerson[variable].age);
+  if(isObjectEmpty(newPerson)) {
+    console.log("nothing in here")
+  }
+  else {
+    for (var variable in newPerson) {
+      console.log("name: " + newPerson[variable].name);
+      console.log("age: " + newPerson[variable].age);
+    }
+  }
+}
+
+function AlreadyIn(someObject,valueToCompare) {
+  var newArray = Object.values(someObject);
+  for(var i=0;i<newArray.length;i++) {
+    var secondArray = Object.values(newArray[i]);
+    for(let shit in secondArray) {
+      if(secondArray[shit] === valueToCompare) {
+        return true;
+      }
+     }
+  }
+  return false;
+}
+//Check if any object is empty
+function isObjectEmpty(Object) {
+  if(Object.length === 0 ) {
+    return true;
+  }
+  else {
+    return false;
   }
 }
 
@@ -32,9 +68,14 @@ function addElement(text) {
   $("#popupID").click(function() {$("#popupID").remove();});
 }
 
-function divAlive() {
+function dieAlive() {
   if($(".popup").length > 0) {
     $(".popup").remove();
   }
 }
 
+$("#textBox").keypress(function(event) {
+ if(event.key === "Enter") {
+  $("#martefButton").click();
+ }
+});
